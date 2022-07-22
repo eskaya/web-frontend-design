7bdf699989837d3612dc5fbd658548baac4c3d2b
eskaya1 <esra.kaya@naylalabs.com>
2022-07-22 13:57:15 +0300

    create project.

---
+++ script.js
@@ -0,0 +1,18 @@
+var navLinks = document.getElementById('navLinks');
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+function showMenu() {
+    navLinks.style.right = "0px";
+}
+
+let newProductName;
+let newUnitPrice;
+let newQuantity;
+
+/*
+({productName: newProductName, unitPrice: newUnitPrice, quantity: newQuantity} = {productName:"Elma", unitPrice:"60", quantity: "4"});
+console.log(newProductName);
+ */
+
+
